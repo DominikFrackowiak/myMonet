bae78df775703a7db65a98004db1ab2a92a3e797
DominikFrackowiak <rorschach@op.pl>
2023-03-18 11:52:59 +0100

    route guarding done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Signup from './pages/signup/Signup'
 import Login from './pages/login/Login'
 import Navbar from './components/Navbar'
+import { useAuthContext } from './hooks/useAuthContext'
 
 function App() {
+	const { authIsReady } = useAuthContext()
+	const { user } = useAuthContext()
+
 	return (
 		<div className='App'>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/signup' element={<Signup />} />
-					<Route path='/login' element={<Login />} />
-				</Routes>
-			</BrowserRouter>
+			{authIsReady && (
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route
+							path='/'
+							element={user ? <Home /> : <Navigate to='/login' />}
+						/>
+						<Route
+							path='/signup'
+							element={!user ? <Signup /> : <Navigate to='/' />}
+						/>
+						<Route
+							path='/login'
+							element={!user ? <Login /> : <Navigate to='/' />}
+						/>
+					</Routes>
+				</BrowserRouter>
+			)}
 		</div>
 	)
 }
